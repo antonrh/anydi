73b888beb44e91087aeca0e5920c37410e8bb98a
antonrh <antonruhlov@gmail.com>
2023-03-01 14:10:17 +0200

    Add basic example

---
+++ examples/basic/main.py
@@ -0,0 +1,21 @@
+import pyxdi
+
+di = pyxdi.PyxDI()
+di.scan(["examples.basic"])
+
+
+def main() -> None:
+    from examples.basic.handlers import create_user, get_user, get_users
+
+    di.start()
+
+    user = create_user(email="demo@mail.com")
+
+    assert get_users() == [user]
+    assert get_user(email="demo@mail.com") == user
+
+    di.close()
+
+
+if __name__ == "__main__":
+    main()
