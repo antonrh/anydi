eacdb8fe1b53e0b6b5436d469f7bf956e34e2303
antonrh <antonruhlov@gmail.com>
2023-06-15 22:39:19 +0300

    Remove is_builtin_type

--- tests/test_utils.py
+++ tests/test_utils.py
@@ -2,7 +2,7 @@ import typing as t
 
 import pytest
 
-from pyxdi.utils import get_full_qualname, is_builtin_type
+from pyxdi.utils import get_full_qualname
 
 from tests.fixtures import Service
 
@@ -22,17 +22,3 @@ def test_get_full_qualname(obj: t.Any, expected_qualname: str) -> None:
     qualname = get_full_qualname(obj)
 
     assert qualname == expected_qualname
-
-
-@pytest.mark.parametrize(
-    "tp, expected",
-    [
-        (bool, True),
-        (str, True),
-        (int, True),
-        (float, True),
-        (Service, False),
-    ],
-)
-def test_is_builtin_type(tp: t.Type[t.Any], expected: bool) -> None:
-    assert is_builtin_type(tp) == expected
