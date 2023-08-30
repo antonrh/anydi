7fc2d44f076e81a88ebe4c91316f209b0150f30a
antonrh <antonruhlov@gmail.com>
2023-08-30 21:18:38 +0300

    Import Provider class from package root

--- pyxdi/__init__.py
+++ pyxdi/__init__.py
@@ -1,5 +1,5 @@
 """PyxDI public objects and functions."""
-from .core import Module, PyxDI, dep, named
+from .core import Module, Provider, PyxDI, dep, named
 from .decorators import inject, provider
 
-__all__ = ["PyxDI", "Module", "named", "dep", "inject", "provider"]
+__all__ = ["PyxDI", "Module", "named", "dep", "inject", "provider", "Provider"]
