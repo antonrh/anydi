37fe73bc17c7c8190dd1ba2095c990aac5c2cfde
antonrh <antonruhlov@gmail.com>
2024-01-26 16:16:40 +0200

    Removed Named type

--- pyxdi/__init__.py
+++ pyxdi/__init__.py
@@ -1,12 +1,11 @@
 """PyxDI public objects and functions."""
-from .core import Module, Named, Provider, PyxDI, Scope, dep
+from .core import Module, Provider, PyxDI, Scope, dep
 from .decorators import inject, provider, request, singleton, transient
 
 __all__ = [
     "dep",
     "inject",
     "Module",
-    "Named",
     "provider",
     "Provider",
     "PyxDI",
