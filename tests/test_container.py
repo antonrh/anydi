7c2ee52f1eee935a06f334d47db44c2c2626baf0
antonrh <antonruhlov@gmail.com>
2024-02-25 08:39:06 +0200

    Move provider tests

--- tests/test_container.py
+++ tests/test_container.py
@@ -31,64 +31,6 @@ def test_default_strict(container: Container) -> None:
     assert container.strict
 
 
-# Provider
-
-
-def test_provider_function_type() -> None:
-    provider = Provider(obj=lambda: "test", scope="transient")
-
-    assert provider.is_function
-    assert not provider.is_class
-    assert not provider.is_generator
-    assert not provider.is_async_generator
-
-
-def test_provider_function_class() -> None:
-    provider = Provider(obj=Service, scope="transient")
-
-    assert not provider.is_function
-    assert provider.is_class
-    assert not provider.is_generator
-    assert not provider.is_async_generator
-
-
-def test_provider_function_resource() -> None:
-    def resource() -> t.Iterator[str]:
-        yield "test"
-
-    provider = Provider(obj=resource, scope="transient")
-
-    assert not provider.is_function
-    assert not provider.is_class
-    assert provider.is_generator
-    assert not provider.is_async_generator
-
-
-def test_provider_function_async_resource() -> None:
-    async def resource() -> t.AsyncIterator[str]:
-        yield "test"
-
-    provider = Provider(obj=resource, scope="transient")
-
-    assert not provider.is_function
-    assert not provider.is_class
-    assert not provider.is_generator
-    assert provider.is_async_generator
-
-
-def test_provider_name() -> None:
-    def obj() -> str:
-        return "test"
-
-    provider = Provider(obj=obj, scope="transient")
-
-    assert (
-        provider.name
-        == str(provider)
-        == "tests.test_container.test_provider_name.<locals>.obj"
-    )
-
-
 def test_has_provider(container: Container) -> None:
     container.register_provider(str, lambda: "test", scope="singleton")
 
