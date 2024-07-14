c1a1d8b278f9777f4971722be7bc5288e343218a
antonrh <antonruhlov@gmail.com>
2024-07-14 15:56:16 +0300

    Add Unit Tests for Resource Exception Delegation

--- tests/fixtures.py
+++ tests/fixtures.py
@@ -2,3 +2,19 @@ class Service:
     def __init__(self, ident: str) -> None:
         self.ident = ident
         self.events: list[str] = []
+
+
+class Resource:
+    def __init__(self) -> None:
+        self.called = False
+        self.committed = False
+        self.rolled_back = False
+
+    def run(self) -> None:
+        self.called = True
+
+    def commit(self) -> None:
+        self.committed = True
+
+    def rollback(self) -> None:
+        self.rolled_back = True
