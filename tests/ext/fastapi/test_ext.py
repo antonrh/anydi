f959389f8ef32c8c558306b7fe876ec396180a8f
antonrh <antonruhlov@gmail.com>
2025-02-05 16:09:54 +0200

    Update extensions tests

--- tests/ext/fastapi/test_ext.py
+++ tests/ext/fastapi/test_ext.py
@@ -10,10 +10,8 @@ from anydi.ext.fastapi import Inject, install
 def test_inject_param_missing_interface() -> None:
     param = Inject()
 
-    with pytest.raises(TypeError) as exc_info:
+    with pytest.raises(TypeError, match="Interface is not set."):
         _ = param.interface
-
-    assert str(exc_info.value) == "Interface is not set."
 
 
 def test_install_without_annotation() -> None:
@@ -29,13 +27,10 @@ def test_install_without_annotation() -> None:
     def say_hello(message=Inject()) -> Any:  # type: ignore[no-untyped-def]
         return message
 
-    with pytest.raises(TypeError) as exc_info:
+    with pytest.raises(
+        TypeError, match="Missing `(.*?).say_hello` parameter `message` annotation."
+    ):
         install(app, container)
-
-    assert str(exc_info.value) == (
-        "Missing `tests.ext.fastapi.test_ext.test_install_without_annotation"
-        ".<locals>.say_hello` parameter `message` annotation."
-    )
 
 
 def test_install_unknown_annotation() -> None:
@@ -47,11 +42,11 @@ def test_install_unknown_annotation() -> None:
     def say_hello(message: str = Inject()) -> Any:
         return message
 
-    with pytest.raises(LookupError) as exc_info:
+    with pytest.raises(
+        LookupError,
+        match=(
+            "`(.*?).say_hello` has an unknown dependency parameter `message` "
+            "with an annotation of `str`."
+        ),
+    ):
         install(app, container)
-
-    assert str(exc_info.value) == (
-        "`tests.ext.fastapi.test_ext.test_install_unknown_annotation"
-        ".<locals>.say_hello` has an unknown dependency parameter `message` "
-        "with an annotation of `str`."
-    )
