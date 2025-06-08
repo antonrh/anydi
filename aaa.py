3d23c7de6a40964bc8e89bd5b4666e809253d709
antonrh <antonruhlov@gmail.com>
2025-06-08 08:19:50 +0300

    Remove aaa.py

--- aaa.py
+++
@@ -1,9 +0,0 @@
-from anydi import Container
-from anydi.testcontainer import TestContainer
-
-
-def init_container(testing: bool = False) -> Container:
-    container = Container()
-    if testing:
-        return TestContainer.from_container(container)
-    return container
