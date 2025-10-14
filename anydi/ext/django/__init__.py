c1e48d967badc122fd96d39353a6321d8842ef80
antonrh <antonruhlov@gmail.com>
2025-10-14 18:12:47 +0300

    Add Django ImportError

---
+++ anydi/ext/django/__init__.py
@@ -0,0 +1,6 @@
+raise ImportError(
+    "Django extension requires additional dependencies. "
+    "Install them with one of the following:\n\n"
+    "  pip install 'anydi[django]'     # for Django\n"
+    "  pip install 'anydi[ninja]'      # for Django Ninja\n"
+)
