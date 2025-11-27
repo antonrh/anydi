5ea4690b5af1263e97e8cd1edd835b25dd778270
antonrh <antonruhlov@gmail.com>
2025-11-27 21:55:24 +0200

    Move injector related methods to _injector.py

--- anydi/_container.py
+++ anydi/_container.py
@@ -3,7 +3,6 @@
 from __future__ import annotations
 
 import contextlib
-import functools
 import inspect
 import logging
 import types
@@ -11,12 +10,13 @@ import uuid
 from collections import defaultdict
 from collections.abc import AsyncIterator, Callable, Iterable, Iterator
 from contextvars import ContextVar
-from typing import Annotated, Any, TypeVar, cast, get_args, get_origin, overload
+from typing import Any, TypeVar, get_args, get_origin, overload
 
 from typing_extensions import ParamSpec, Self, type_repr
 
 from ._context import InstanceContext
 from ._decorators import is_provided
+from ._injector import Injector
 from ._module import ModuleDef, ModuleRegistrar
 from ._provider import Provider, ProviderDef, ProviderKind, ProviderParameter
 from ._resolver import Resolver
@@ -26,7 +26,6 @@ from ._types import (
     Event,
     Scope,
     is_event_type,
-    is_inject_marker,
     is_iterator_type,
     is_none_type,
 )
@@ -58,10 +57,10 @@ class Container:
         self._request_context_var: ContextVar[InstanceContext | None] = ContextVar(
             "request_context", default=None
         )
-        self._inject_cache: dict[Callable[..., Any], Callable[..., Any]] = {}
 
         # Components
         self._resolver = Resolver(self)
+        self._injector = Injector(self)
         self._modules = ModuleRegistrar(self)
         self._scanner = Scanner(self)
 
@@ -548,7 +547,7 @@ class Container:
         """Decorator to inject dependencies into a callable."""
 
         def decorator(call: Callable[P, T]) -> Callable[P, T]:
-            return self._inject(call)
+            return self._injector.inject(call)
 
         if func is None:
             return decorator
@@ -556,107 +555,13 @@ class Container:
 
     def run(self, func: Callable[P, T], /, *args: P.args, **kwargs: P.kwargs) -> T:
         """Run the given function with injected dependencies."""
-        return self._inject(func)(*args, **kwargs)
-
-    def _inject(self, call: Callable[P, T]) -> Callable[P, T]:
-        """Inject dependencies into a callable."""
-        if call in self._inject_cache:
-            return cast(Callable[P, T], self._inject_cache[call])
-
-        injected_params = self._get_injected_params(call)
-        if not injected_params:
-            self._inject_cache[call] = call
-            return call
-
-        if inspect.iscoroutinefunction(call):
-
-            @functools.wraps(call)
-            async def awrapper(*args: P.args, **kwargs: P.kwargs) -> T:
-                for name, annotation in injected_params.items():
-                    kwargs[name] = await self.aresolve(annotation)
-                return cast(T, await call(*args, **kwargs))
-
-            self._inject_cache[call] = awrapper
-
-            return awrapper  # type: ignore
-
-        @functools.wraps(call)
-        def wrapper(*args: P.args, **kwargs: P.kwargs) -> T:
-            for name, annotation in injected_params.items():
-                kwargs[name] = self.resolve(annotation)
-            return call(*args, **kwargs)
-
-        self._inject_cache[call] = wrapper
-
-        return wrapper
-
-    def _get_injected_params(self, call: Callable[..., Any]) -> dict[str, Any]:
-        """Get the injected parameters of a callable object."""
-        injected_params: dict[str, Any] = {}
-        for parameter in inspect.signature(call, eval_str=True).parameters.values():
-            interface, should_inject = self.validate_injected_parameter(
-                parameter, call=call
-            )
-            if should_inject:
-                injected_params[parameter.name] = interface
-        return injected_params
-
-    @staticmethod
-    def _unwrap_injected_parameter(parameter: inspect.Parameter) -> inspect.Parameter:
-        if get_origin(parameter.annotation) is not Annotated:
-            return parameter
-
-        origin, *metadata = get_args(parameter.annotation)
-
-        if not metadata or not is_inject_marker(metadata[-1]):
-            return parameter
-
-        if is_inject_marker(parameter.default):
-            raise TypeError(
-                "Cannot specify `Inject` in `Annotated` and "
-                f"default value together for '{parameter.name}'"
-            )
-
-        if parameter.default is not inspect.Parameter.empty:
-            return parameter
-
-        marker = metadata[-1]
-        new_metadata = metadata[:-1]
-        if new_metadata:
-            if hasattr(Annotated, "__getitem__"):
-                new_annotation = Annotated.__getitem__((origin, *new_metadata))  # type: ignore
-            else:
-                new_annotation = Annotated.__class_getitem__((origin, *new_metadata))  # type: ignore
-        else:
-            new_annotation = origin
-        return parameter.replace(annotation=new_annotation, default=marker)
+        return self._injector.inject(func)(*args, **kwargs)
 
     def validate_injected_parameter(
         self, parameter: inspect.Parameter, *, call: Callable[..., Any]
     ) -> tuple[Any, bool]:
         """Validate an injected parameter."""
-        parameter = self._unwrap_injected_parameter(parameter)
-        interface = parameter.annotation
-
-        if not is_inject_marker(parameter.default):
-            return interface, False
-
-        if interface is inspect.Parameter.empty:
-            raise TypeError(
-                f"Missing `{type_repr(call)}` parameter `{parameter.name}` annotation."
-            )
-
-        # Set inject marker interface
-        parameter.default.interface = interface
-
-        if not self.has_provider_for(interface):
-            raise LookupError(
-                f"`{type_repr(call)}` has an unknown dependency parameter "
-                f"`{parameter.name}` with an annotation of "
-                f"`{type_repr(interface)}`."
-            )
-
-        return interface, True
+        return self._injector.validate_parameter(parameter, call=call)
 
     # == Module Registration ==
 
