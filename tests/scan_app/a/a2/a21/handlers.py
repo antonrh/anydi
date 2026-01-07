aafae1b0ba285f55986b9024b9cb06ee72dd2759
antonrh <antonruhlov@gmail.com>
2026-01-07 23:38:36 +0200

    Replace tests auto marker with Inject()

--- tests/scan_app/a/a2/a21/handlers.py
+++ tests/scan_app/a/a2/a21/handlers.py
@@ -1,6 +1,6 @@
-from anydi import auto, injectable
+from anydi import Inject, injectable
 
 
 @injectable(tags=["a", "a2", "a21"])
-def a21_handler(message: str = auto) -> None:
+def a21_handler(message: str = Inject()) -> None:
     pass
--- tests/scan_app/a/a3/handlers.py
+++ tests/scan_app/a/a3/handlers.py
@@ -1,12 +1,12 @@
-from anydi import auto, injectable
+from anydi import Inject, injectable
 
 from tests.fixtures import Service
 
 
 @injectable(tags=["inject"])
-def a_a3_handler_1(message: str = auto) -> str:
+def a_a3_handler_1(message: str = Inject()) -> str:
     return message
 
 
-def a_a3_handler_2(service: Service = auto) -> Service:
+def a_a3_handler_2(service: Service = Inject()) -> Service:
     return service
